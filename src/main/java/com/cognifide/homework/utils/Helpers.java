a2c55f8ca7e5aca7f0309aea456b29cae11f99d4
Michał Chyb <michalchyb1@gmail.com>
2019-06-13 21:37:32 +0200

    code refactor

--- src/main/java/com/cognifide/homework/utils/Helpers.java
+++ src/main/java/com/cognifide/homework/utils/Helpers.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
 
 public class Helpers {
 
@@ -16,7 +15,7 @@ public class Helpers {
 
     public static Book getJsonFromFile() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(new File(Objects.requireNonNull(Helpers.class.getClassLoader().getResource("books.json")).getFile()), Book.class);
+        return mapper.readValue(new File(Helpers.class.getClassLoader().getResource("books.json").getFile()), Book.class);
     }
 
 
